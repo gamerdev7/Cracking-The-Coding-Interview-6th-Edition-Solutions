4fdf6b989bb4d4b30bea3591976af7633ce37738
gamerdev <gamerdev@users.noreply.github.com>
2022-02-23 20:03:39 +0530

    StringRotation

---
+++ Chapter-01 Array and Strings/StringRotation/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    // time: O(Length(a))
+    // space: O(1)
+    public static bool StringRotation(string a, string b)
+    {
+        if (a.Length != b.Length && a.Length < 1) return false;
+
+        return IsSubstring(a + a, b);
+    }
+
+
+    // Suppose this method is already given
+    private static bool IsSubstring(string a, string b)
+    {
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        string a = "waterbottle";
+        string b = "erbottlewat";
+        Console.WriteLine(StringRotation(a, b));
+    }
+}
